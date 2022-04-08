274ae5358fee03768e07d3e8759f7d0d2061609c
nguyenvietanh1409 <vietanh20030914@gmail.com>
2022-04-08 13:49:01 +0700

    lsdlisfdg

---
+++ src/com/company/PrintD.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class PrintD {
+    public static void main(String[] args)
+    {
+        System.out.println("    *     ");
+        System.out.println("* *   * * ");
+        System.out.println("   * *    ");
+        System.out.println("  *   *   ");
+        System.out.println(" *     *  ");
+    }
+}
---
+++ src/com/company/PrintSheep.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class PrintSheep {
+    public static void main (String[] args)
+    {
+        System.out.println("          '  ' ");
+        System.out.println("           __  ");
+        System.out.println("          (oo) ");
+        System.out.println("  /========//  ");
+        System.out.println(" / || @@ ||    ");
+        System.out.println("*  ||----||    ");
+        System.out.println("   VV    VV    ");
+        System.out.println("   ''    ''    ");
+    }
+}
